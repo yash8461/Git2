d83fb871f324d02921d04a36145acbfa17577931
Yash Malviya <malviyash8461@gmail.com>
2023-03-18 19:25:09 +0530

    commit

---
+++ Typescript/day1/index.js
@@ -0,0 +1,8 @@
+var employees = [
+    { ID: 1, FirstName: "Ajay", LastName: "singh", Address: "India", Salary: 8000 },
+    { ID: 2, FirstName: "Vivek", LastName: "Soni", Address: "USA", Salary: 9000 },
+    { ID: 3, FirstName: "Shivam", LastName: "Borban", Address: "Canada", Salary: 19000 },
+    { ID: 4, FirstName: "Rahul", LastName: "Yadav", Address: "Chine", Salary: 20000 },
+    { ID: 5, FirstName: "Raj", LastName: "singh", Address: "Pakistan", Salary: 30000 }
+];
+console.log(employees[2].ID);
---
+++ Typescript/day1/index.ts
@@ -0,0 +1,11 @@
+ let  employees : any =[
+    {ID:1, FirstName:"Ajay", LastName:"singh", Address:"India", Salary: 8000 },
+    {ID:2, FirstName:"Vivek", LastName:"Soni", Address:"USA", Salary: 9000 },
+    {ID:3, FirstName:"Shivam", LastName:"Borban", Address:"Canada", Salary: 19000 },
+    {ID:4, FirstName:"Rahul", LastName:"Yadav", Address:"Chine", Salary: 20000 },
+    {ID:5, FirstName:"Raj", LastName:"singh", Address:"Pakistan", Salary:30000 }
+
+ ]
+
+ var l = 1;
+ console.log(employees[2].ID)
