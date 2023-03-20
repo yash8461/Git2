e72eee458685938cb7ba5840b067c42d584d30e7
Yash Malviya <malviyash8461@gmail.com>
2023-03-20 19:32:59 +0530

    comy

---
+++ Typescript/day1/Assign5.js
@@ -0,0 +1,26 @@
+var employee = [
+    { ID: 231, Name: "Ajay", city: "Bhopal", DOJ: "10 / 10 / 2021" },
+    { ID: 531, Name: "Shivam", city: "Indore", DOJ: "12 / 10 / 2016" },
+    { ID: 601, Name: "Rishi", city: "Ahmedad", DOJ: "5 / 10 / 2023" },
+    { ID: 721, Name: "Raj", city: "surat", DOJ: "22/ 10 / 2019" },
+    { ID: 581, Name: "Nishant", city: "Banglore", DOJ: "10/ 10 / 2022" },
+];
+//  Searching the employee with ID
+//  var k =721;
+//   for (let i = 0; i < employee.length; i++) {
+//    if(employee[i].ID==k){
+//  console.log("ID: "+ employee[i].ID)
+//  console.log("Name: "+ employee[i].Name)
+//  console.log( "city: "+ employee[i].city)
+//  console.log( "DOJ: "+ employee[i].DOJ)
+//    }
+//   }
+// Searching the employees who has joined after year 2020
+for (var i = 0; i < employee.length; i++) {
+    if (employee[i].DOJ > " / / 2020 ") {
+        console.log("ID: " + employee[i].ID);
+        console.log("Name: " + employee[i].Name);
+        console.log("city: " + employee[i].city);
+        console.log("DOJ: " + employee[i].DOJ);
+    }
+}
--- Typescript/day1/index.ts
+++ Typescript/day1/index.ts
@@ -1,11 +1,62 @@
  let  employees : any =[
-    {ID:1, FirstName:"Ajay", LastName:"singh", Address:"India", Salary: 8000 },
-    {ID:2, FirstName:"Vivek", LastName:"Soni", Address:"USA", Salary: 9000 },
-    {ID:3, FirstName:"Shivam", LastName:"Borban", Address:"Canada", Salary: 19000 },
-    {ID:4, FirstName:"Rahul", LastName:"Yadav", Address:"Chine", Salary: 20000 },
-    {ID:5, FirstName:"Raj", LastName:"singh", Address:"Pakistan", Salary:30000 }
+    {ID:123, FirstName:"Ajay", LastName:"singh", Address:[{flatNo:23, city:"Indore",state:"MP"}], Salary: 8000 },
+    {ID:2221, FirstName:"Vivek", LastName:"Soni", Address:[{flatNo:86, city:"Bhopal",state:"MP"}], Salary: 9000 },
+    {ID:1344, FirstName:"Shivam", LastName:"Borban", Address:[{flatNo:53, city:"Ahmedabad",state:"Gujrat"}], Salary: 19000 },
+    {ID:224, FirstName:"Rahul", LastName:"Yadav", Address:[{flatNo:33, city:"Surat",state:"Gujrat"}], Salary: 20000 },
+    {ID:555, FirstName:"Raj", LastName:"singh", Address:[{flatNo:43, city:"Banglore",state:"Karnataka"}], Salary:30000 },
 
  ]
 
- var l = 1;
- console.log(employees[2].ID)
+ //    Employee details before push data
+ console.log(" Employee details before push data")
+ console.log(employees)
+
+
+ // Employee details after push data
+  let newData =  {ID:565, FirstName:"Anil", LastName:"Yadav", Address:[{flatNo:46, city:"Vadodara",state:"gujrat"}], Salary:3000 };
+
+  employees.push(newData );
+  console.log("Employee details after push data")
+  console.log(employees)
+
+
+//   Employee details after delete data
+  console.log("Employee details after delete data")
+  employees.pop();
+  console.log(employees)
+ 
+
+  //   Employee details searching by id
+  var k =565;
+
+  for (let i = 0; i < employees.length; i++) {
+   
+   if(employees[i].ID==k){
+ console.log("ID: "+ employees[i].ID)
+ console.log("FirstName: "+ employees[i].FirstName)
+ console.log( "LastName: "+ employees[i].LastName)
+ console.log( "Address: \n \t flatNo: "+ employees[i].Address[0].flatNo ,"\n \t city: "+ employees[i].Address[0].city,"\n \t state: "+  employees[i].Address[0].state)
+ console.log( "Salary: "+ employees[i].Salary)
+   }
+  }
+
+
+
+//   employee details searching by indexnumber
+ var k = 3;
+
+ console.log("ID: "+ employees[k].ID)
+ console.log("FirstName: "+ employees[k].FirstName)
+ console.log( "LastName: "+ employees[k].LastName)
+ console.log( "Address: \n \t flatNo: "+ employees[k].Address[0].flatNo ,"\n \t city: "+ employees[k].Address[0].city,"\n \t state: "+  employees[k].Address[0].state)
+ console.log( "Salary: "+ employees[k].Salary)
+
+
+ 
+ 
+
+
+
+
+
+
